b6e40306d86f1992ad07340db7265c582fd6b7c9
Jonas Tepe <jonasprogrammer@googlemail.com>
2016-01-02 12:12:59 +0100

    Refactored tests into separate module

---
+++ src/test.rs
@@ -0,0 +1,72 @@
+use super::*;
+
+#[test]
+fn arena_as_intended() {
+    use std::cell::Cell;
+    use std::mem;
+
+    struct DropTracker<'a>(&'a Cell<u32>);
+    impl<'a> Drop for DropTracker<'a> {
+        fn drop(&mut self) {
+            self.0.set(self.0.get() + 1);
+        }
+    }
+
+    struct Node<'a, 'b: 'a>(Option<&'a Node<'a, 'b>>, u32, DropTracker<'b>);
+    let drop_counter = Cell::new(0);
+    {
+        let arena = Arena::with_capacity(2);
+
+        let mut node: &Node = arena.alloc(Node(None, 1, DropTracker(&drop_counter)));
+        assert_eq!(arena.chunks.borrow().rest.len(), 0);
+
+        node = arena.alloc(Node(Some(node), 2, DropTracker(&drop_counter)));
+        assert_eq!(arena.chunks.borrow().rest.len(), 1);
+
+        node = arena.alloc(Node(Some(node), 3, DropTracker(&drop_counter)));
+        assert_eq!(arena.chunks.borrow().rest.len(), 1);
+
+        node = arena.alloc(Node(Some(node), 4, DropTracker(&drop_counter)));
+        assert_eq!(arena.chunks.borrow().rest.len(), 1);
+
+        assert_eq!(node.1, 4);
+        assert_eq!(node.0.unwrap().1, 3);
+        assert_eq!(node.0.unwrap().0.unwrap().1, 2);
+        assert_eq!(node.0.unwrap().0.unwrap().0.unwrap().1, 1);
+        assert   !(node.0.unwrap().0.unwrap().0.unwrap().0.is_none());
+
+        mem::drop(node);
+        assert_eq!(drop_counter.get(), 0);
+
+        let mut node: &Node = arena.alloc(Node(None, 5, DropTracker(&drop_counter)));
+        assert_eq!(arena.chunks.borrow().rest.len(), 1);
+
+        node = arena.alloc(Node(Some(node), 6, DropTracker(&drop_counter)));
+        assert_eq!(arena.chunks.borrow().rest.len(), 2);
+
+        node = arena.alloc(Node(Some(node), 7, DropTracker(&drop_counter)));
+        assert_eq!(arena.chunks.borrow().rest.len(), 2);
+
+        assert_eq!(drop_counter.get(), 0);
+
+        assert_eq!(node.1, 7);
+        assert_eq!(node.0.unwrap().1, 6);
+        assert_eq!(node.0.unwrap().0.unwrap().1, 5);
+        assert   !(node.0.unwrap().0.unwrap().0.is_none());
+
+        assert_eq!(drop_counter.get(), 0);
+
+    }
+    assert_eq!(drop_counter.get(), 7);
+}
+
+#[test]
+fn ensure_into_vec_maintains_order_of_allocation() {
+    let arena = Arena::with_capacity(1);  // force multiple inner vecs
+    for &s in &["t", "e", "s", "t"] {
+        arena.alloc(String::from(s));
+    }
+    let vec = arena.into_vec();
+    assert_eq!(vec, vec!["t", "e", "s", "t"]);
+}
+
